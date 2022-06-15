5f985a9092377b35e6cea1432e57df9ccc2e4707
GuiMatheus1313 <82453465+GuiMatheus1313@users.noreply.github.com>
2022-06-15 00:31:33 -0300

    comentários extras na widget JAVA

--- app/src/main/java/com/example/showdrink1/AbrirHistoricoWidget.java
+++ app/src/main/java/com/example/showdrink1/AbrirHistoricoWidget.java
@@ -15,20 +15,19 @@ public class AbrirHistoricoWidget extends AppWidgetProvider {
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         final int N = appWidgetIds.length;
 
-        // Perform this loop procedure for each App Widget that belongs to this provider
+        // Realizado um loop para cada widget criada 
         for (int i=0; i<N; i++) {
             int appWidgetId = appWidgetIds[i];
 
-            // Create an Intent to launch ExampleActivity
+            // Faz um armazenamento para direcionar o usuário para a MainActivity
             Intent intent = new Intent(context, MainActivity.class);
             PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
 
-            // Get the layout for the App Widget and attach an on-click listener
-            // to the button
+            // Coloca o armazenamento no botão para realizar sua tarefa
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.abrir_historico_widget);
             views.setOnClickPendingIntent(R.id.appwidget_button, pendingIntent);
 
-            // Tell the AppWidgetManager to perform an update on the current app widget
+            // Faz a atualização dos dados da widget pela sua Id
             appWidgetManager.updateAppWidget(appWidgetId, views);
         }
     }
