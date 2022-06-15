36b1c17bbbb65da0220e4c47698d06697aad74a4
GuiMatheus1313 <82453465+GuiMatheus1313@users.noreply.github.com>
2022-06-14 23:56:10 -0300

    Adição de comentários extras na classe NetworkUtils

--- app/src/main/java/com/example/showdrink1/NetworkUtils.java
+++ app/src/main/java/com/example/showdrink1/NetworkUtils.java
@@ -9,24 +9,34 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
+//Classe utilizada para conexão com a API de Drinks
 public class NetworkUtils {
     private static final String LOG_TAG = NetworkUtils.class.getSimpleName();
     // Constantes utilizadas pela API
-    // URL para a API de Livros do Google.
+   
+    // URL que adquire acesso a API.
     private static final String DRINKS_URL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?";
+    
+    
     // Parametros da string de Busca
     private static final String QUERY_PARAM = "s";
-    // Limitador da qtde de resultados
-    //private static final String MAX_RESULTS = "maxResults";
-    // Parametro do tipo de impressão
-    //private static final String TIPO_IMPRESSAO = "printType";
+    /*   DESATIVADOS
+        // Limitador da qtde de resultados
+        //private static final String MAX_RESULTS = "maxResults";
+        // Parametro do tipo de impressão
+        //private static final String TIPO_IMPRESSAO = "printType";
+    
+    */
+    
+    //Utilizado para adaptar o arquivo JSON recebido
     static String buscaInfosLivro(String queryString) {
+        qq
         HttpURLConnection urlConnection = null;
+        //Permite a leitura de texto por meio de uma abertura de transmissão (inputStream)
         BufferedReader reader = null;
         String bookJSONString = null;
         try {
-            // Construção da URI de Busca
+            // Construção da URI para buscar o drink específico
             Uri builtURI = Uri.parse(DRINKS_URL).buildUpon()
                     .appendQueryParameter(QUERY_PARAM, queryString)
                     .build();
@@ -36,7 +46,7 @@ public class NetworkUtils {
             urlConnection = (HttpURLConnection) requestURL.openConnection();
             urlConnection.setRequestMethod("GET");
             urlConnection.connect();
-            // Busca o InputStream.
+            // Busca o InputStream, este que permite a leitura de bytes de um arquivo.
             InputStream inputStream = urlConnection.getInputStream();
             // Cria o buffer para o input stream
             reader = new BufferedReader(new InputStreamReader(inputStream));
