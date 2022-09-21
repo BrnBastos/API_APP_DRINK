56c66ff3daa4bb511fa0a13f2ccf9a53abdecd43
Arthur Renan Gutierrez <82537255+arthurrenan00@users.noreply.github.com>
2022-09-20 21:27:39 -0300

    Colocando os outros métodos POST, DELETE

--- app/src/main/java/com/example/showdrink1/NetworkUtils.java
+++ app/src/main/java/com/example/showdrink1/NetworkUtils.java
@@ -1,7 +1,10 @@
 package com.example.showdrink1;
 
 import android.net.Uri;
+import android.util.JsonReader;
 import android.util.Log;
+
+import com.google.gson.Gson;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -34,7 +37,7 @@ public class NetworkUtils {
         HttpURLConnection urlConnection = null;
         //Permite a leitura de texto por meio de uma abertura de transmissão (inputStream)
         BufferedReader reader = null;
-        String bookJSONString = null;
+        String drinkJSONString = null;
         try {
             // Construção da URI para buscar o drink específico
             Uri builtURI = Uri.parse(DRINKS_URL).buildUpon()
@@ -62,7 +65,7 @@ public class NetworkUtils {
                 // se o stream estiver vazio não faz nada
                 return null;
             }
-            bookJSONString = builder.toString();
+            drinkJSONString = builder.toString();
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
@@ -79,8 +82,210 @@ public class NetworkUtils {
             }
         }
         // escreve o Json no log
-        Log.d(LOG_TAG, bookJSONString);
-        return bookJSONString;
+        Log.d(LOG_TAG, drinkJSONString);
+        return drinkJSONString;
     }
 
+    static void addDrink(DrinkGit drink){
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        String characterJSONString = null;
+
+
+
+        try {
+            Uri builtURI;
+            if(drink == null){
+                builtURI = Uri.parse(DRINKS_URL).buildUpon()
+                        .build();
+            }
+            else {
+                String url1 = DRINKS_URL;
+                //Construção da URI de Busca
+                builtURI = Uri.parse(url1).buildUpon()
+                        .build();
+
+
+            }
+
+            // Busca o InputStream.
+            InputStream inputStream = urlConnection.getInputStream();
+            // Cria o buffer para o input stream
+            InputStreamReader inputStreamReader =
+                    new InputStreamReader(inputStream, "UTF-8");
+
+            JsonReader jsonReader = new JsonReader(inputStreamReader);
+            jsonReader.beginObject();
+            while (jsonReader.hasNext()) {
+                String url = jsonReader.nextName();
+                if (url.equals("URL DA API")) {
+                    Gson gson = new Gson();
+                    String drinkadd = gson.toJson(drink);
+
+                    drinkadd = jsonReader.nextString();
+
+                    break;
+                } else {
+                    jsonReader.skipValue();
+                }
+            }
+            // Converte a URI para a URL.
+            URL requestURL = new URL(builtURI.toString());
+            // Abre a conexão de rede
+            urlConnection = (HttpURLConnection) requestURL.openConnection();
+            urlConnection.setRequestMethod("POST");
+            urlConnection.connect();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // fechando a conexão e o buffer.
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    static void updateDrink(DrinkGit drink){
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        String characterJSONString = null;
+
+
+
+        try {
+            Uri builtURI;
+            if(drink == null){
+                builtURI = Uri.parse(DRINKS_URL).buildUpon()
+                        .build();
+            }
+            else {
+                String url1 = DRINKS_URL;
+                //Construindo a URI de Busca
+                builtURI = Uri.parse(url1).buildUpon()
+                        .build();
+
+
+            }
+
+            // Buscando o InputStream.
+            InputStream inputStream = urlConnection.getInputStream();
+            // Cria o buffer para o input stream
+            InputStreamReader inputStreamReader =
+                    new InputStreamReader(inputStream, "UTF-8");
+
+            JsonReader jsonReader = new JsonReader(inputStreamReader);
+            jsonReader.beginObject();
+            while (jsonReader.hasNext()) {
+                String url = jsonReader.nextName();
+                if (url.equals("URL DA API")) {
+                    Gson gson = new Gson();
+                    String drinkupdate = gson.toJson(drink);
+
+                    drinkupdate = jsonReader.nextString();
+
+                    break;
+                } else {
+                    jsonReader.skipValue();
+                }
+            }
+            // Converte a URI para a URL.
+            URL requestURL = new URL(builtURI.toString());
+            // Abre a conexão de rede
+            urlConnection = (HttpURLConnection) requestURL.openConnection();
+            urlConnection.setRequestMethod("SET");
+            urlConnection.connect();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // fecha a conexão e o buffer.
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    static void deleteDrink(String drink) {
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        String characterJSONString = null;
+
+
+        try {
+            Uri builtURI;
+            if (drink == null) {
+                builtURI = Uri.parse(DRINKS_URL).buildUpon()
+                        .build();
+            } else {
+                String url1 = DRINKS_URL;
+                //Construção da URI de Busca
+                builtURI = Uri.parse(url1).buildUpon()
+                        .build();
+
+
+            }
+
+            // Busca o InputStream.
+            InputStream inputStream = urlConnection.getInputStream();
+            // Cria o buffer para o input stream
+            InputStreamReader inputStreamReader =
+                    new InputStreamReader(inputStream, "UTF-8");
+
+            JsonReader jsonReader = new JsonReader(inputStreamReader);
+            jsonReader.beginObject(); // processando o JSON object
+            while (jsonReader.hasNext()) {
+                String url = jsonReader.nextName();
+                if (url.equals("URL DA API")) {
+                    Gson gson = new Gson();
+                    String drinkupdate = gson.toJson(drink);
+
+                    drinkupdate = jsonReader.nextString();
+
+
+                    break;
+                } else {
+                    jsonReader.skipValue();
+                }
+            }
+            // Converte a URI para a URL.
+            URL requestURL = new URL(builtURI.toString());
+            // Abre a conexão de rede
+            urlConnection = (HttpURLConnection) requestURL.openConnection();
+            urlConnection.setRequestMethod("DELETE");
+            urlConnection.connect();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // fecha a conexão e o buffer.
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
